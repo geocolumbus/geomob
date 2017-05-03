90a2507c5b5db9b0e07533fde8d3996ade9f7325
George Campbell <campbelg@oclc.org>
2017-05-03 14:00:29 -0400

    Implement water level application.

--- www/js/app/wiggle.js
+++ www/js/app/wiggle.js
@@ -2,7 +2,7 @@ define(["jquery"], function($) {
 
     var _go = function() {
 
-        var panel = $('.panel'),
+        var water = $('.water'),
             h1 = $('h1');
 
         /* Device */
@@ -12,17 +12,49 @@ define(["jquery"], function($) {
 
         /* Acceleration *************************************************************/
 
+        var smoothedAccelerationX = 0,
+            smoothedAccelerationY = 0,
+            deviceRotationAngle = 0,
+            rotationAngleAdder = 1,
+            cssAngle;
+
         function onSuccess(acceleration) {
-            var zDegrees = 9 * acceleration.x / 2,
-                xDegrees = 0,//9 * acceleration.y,
-                yDegrees = 0//9 * acceleration.z;
+            smoothedAccelerationX = (0.9 * smoothedAccelerationX + 0.1 * acceleration.x);
+            smoothedAccelerationY = (0.9 * smoothedAccelerationY + 0.1 * acceleration.y);
 
-            $('.acceleration-x').html(xDegrees);
-            $('.acceleration-y').html(yDegrees);
-            $('.acceleration-z').html(zDegrees);
+            // Vary the rotation angle between -5 and +5 if in the browser
+            if (device.platform === "browser") {
+                deviceRotationAngle += rotationAngleAdder;
+                if (deviceRotationAngle > 5) {
+                    rotationAngleAdder = -1;
+                }
+                if (deviceRotationAngle < -5) {
+                    rotationAngleAdder = 1;
+                }
+            } else {
+                deviceRotationAngle = Math.atan2(smoothedAccelerationX, smoothedAccelerationY) * 180 / 3.14159;
+            }
 
-            panel.css({
-                "transform": "rotateX(" + xDegrees + "deg) rotateY(" + yDegrees + "deg) rotateZ(" + zDegrees + "deg)"
+            if (window.orientation == 0) {
+                cssAngle = deviceRotationAngle;
+            }
+
+            if (window.orientation == -90) {
+                cssAngle = -90 + deviceRotationAngle;
+            }
+
+            if (window.orientation == 90) {
+                cssAngle = 90 + deviceRotationAngle;
+            }
+
+            var formattedRotationAngle = deviceRotationAngle.toFixed(0) + '';
+            while (formattedRotationAngle.length < 4) {
+                formattedRotationAngle = "&nbsp;" + formattedRotationAngle;
+            }
+
+            $('.rotation-angle').html(formattedRotationAngle);
+            water.css({
+                "transform": "rotateZ(" + cssAngle + "deg)"
             });
         }
 
@@ -35,7 +67,7 @@ define(["jquery"], function($) {
 
         setInterval(function() {
             navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-        }, 100);
+        }, 50);
 
     }
 
